5a2e91636fef532242139980ac798bc58bca19d0
Aleksey Yuriev <alex11-90@yandex.by>
2024-06-06 17:53:24 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="page">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  min-width: 320px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
